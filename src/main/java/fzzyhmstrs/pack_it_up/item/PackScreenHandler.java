949344509ce626c339433099233329ce6dd78f86
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-12-12 21:15:26 -0500

    fix cherry-picked name change

--- src/main/java/fzzyhmstrs/pack_it_up/item/PackScreenHandler.java
+++ src/main/java/fzzyhmstrs/pack_it_up/item/PackScreenHandler.java
@@ -63,7 +63,7 @@ public class PackScreenHandler extends ScreenHandler {
     }
 
     @Override
-    public ItemStack quickMove(PlayerEntity player, int index) {
+    public ItemStack transferSlot(PlayerEntity player, int index) {
         ItemStack itemStack = ItemStack.EMPTY;
         Slot slot = this.slots.get(index);
         if (slot.hasStack()) {
